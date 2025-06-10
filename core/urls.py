73ba4cf3286f34ebc29e4cec0407c9f0e52bf218
Jimmy Martinez <jimmy.martinez@smartquick.com.co>
2025-06-09 22:23:02 -0500

    erased views.py from core

--- core/urls.py
+++ core/urls.py
@@ -19,10 +19,8 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
-from .views import TestView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test/', TestView.as_view()),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
